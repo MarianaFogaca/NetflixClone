721c61db5f9bf7d7e6f3bbb80d3e83ca082234b3
Mariana <56118101+MarianaFogaca@users.noreply.github.com>
2020-10-06 14:38:35 -0300

    Add files via upload

---
+++ Digital Innovation One/JavaScript/main.js
@@ -0,0 +1,17 @@
+
+function soma(n1, n2){
+	return n1 + n2;
+}
+
+/*
+var validar = 0;
+
+function validaIdade(idade){
+	if (idade >= 18){
+		validar = true
+	}else{
+		validar = false
+	}
+	return validar;
+}
+*/
